5dfca75a4f1b65f31f5edac41e9f599300ea272b
Brendon Forsgren <bforsgren29@gmail.com>
2019-02-23 12:51:28 -0700

    Converted RGB to hsv for color detection

--- ball_detection.cpp
+++ ball_detection.cpp
@@ -1,7 +1,21 @@
 #include <opencv2/opencv.hpp>
 
+// Hue values of basic colors from https://www.opencv-srf.com/2010/09/object-detection-using-color-seperation.html
+//
+//         Orange  0-22
+//         Yellow 22- 38
+//         Green 38-75
+//         Blue 75-130
+//         Violet 130-160
+//         Red 160-179
+
+
 int main()
 {
+  cv::Mat img, hsv;
+  img = cv::imread("plinko.jpg");
+
+  cv::cvtColor(img, hsv, cv::COLOR_BGR2HSV); //Easier to detect color in HSV
 
   return 0;
 }
