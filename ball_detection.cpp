9b1324a03333b8e94ac5875563817e93cc47372a
Brendon Forsgren <bforsgren29@gmail.com>
2019-02-25 21:45:35 -0700

    Now I detect the centroids of each color ball. Just need to convert pixel x location to the a column number

--- ball_detection.cpp
+++ ball_detection.cpp
@@ -1,15 +1,57 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <vector>
 #include <iostream>
 
 // Hue values of basic colors from https://www.opencv-srf.com/2010/09/object-detection-using-color-seperation.html
 //
 // On test img: values may need to be adjusted due to glare
+// These values may work for RGB
 //  Red: H:0-120, S: 0-122, V: 189-255
 //  Blue: H: 170-179, S: 150-205:, V: 120-160
 // Green: H: 107-179, S: 200-255, V:0-130
 
+// These values are for HSV: Note that these may need to be adjusted for glare
+//  Red: H:0-40, S:130-255, V: 215-255
+//  Blue: H: 95-135, S:70-255, V:185-255
+// Green: H: 50-90, S:80-180, V: 170-255
+
+cv::Mat cleanUpNoise(cv::Mat noisy_img)
+{
+  cv::Mat img;
+  cv::Mat element = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5, 5)); //Maybe make this 3, 3
+  cv::erode(noisy_img, img, element);
+  cv::dilate(img, img, element);
+
+  return img;
+}
+
+std::vector<cv::Point2f> findCentroids(cv::Mat diff)
+{
+  //For some reason this finds the centroid twice
+  cv::Mat canny_out;
+  cv::Canny(diff, canny_out, 100, 200, 3); //May need to change the middle two values
+  std::vector<std::vector<cv::Point>> contours;
+  cv::findContours(canny_out, contours, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
+
+  std::vector<cv::Moments> mu(contours.size());
+  std::vector<cv::Point2f> mc(contours.size());
+  for(int i(0); i< contours.size(); i++)
+  {
+    mu[i] = cv::moments(contours[i]);
+    mc[i] = cv::Point2f(static_cast<float>(mu[i].m10/(mu[i].m00+1e-5)),
+                    static_cast<float>(mu[i].m01/(mu[i].m00+1e-5)));
+  }
+
+  return mc;
+}
+
+void printCentroids(std::vector<cv::Point2f> pts)
+{
+  for(cv::Point2f pt : pts)
+    std::cout << pt << std::endl;
+}
 
 int main()
 {
@@ -34,18 +76,46 @@ int main()
   // int blue_low(75), blue_high(130);
   // int green_low(38), green_high(75);
 
-  while(true)
-  {
-    // cap >> img;
-    cv::cvtColor(img, hsv, cv::COLOR_BGR2HSV); //Easier to detect color in HSV
-    // cv::inRange(img, cv::Scalar(red_low, low_s, low_v), cv::Scalar(red_high, high_s, high_v), img_red);
-    cv::inRange(img, cv::Scalar(low_h, low_s, low_v), cv::Scalar(high_h, high_s, high_v), img_red);
+  //For tuning
+  // while(true)
+  // {
+  //   // cap >> img;
+  //   cv::cvtColor(img, hsv, cv::COLOR_BGR2HSV); //Easier to detect color in HSV
+  //   // cv::inRange(img, cv::Scalar(red_low, low_s, low_v), cv::Scalar(red_high, high_s, high_v), img_red);
+  //   cv::inRange(hsv, cv::Scalar(low_h, low_s, low_v), cv::Scalar(high_h, high_s, high_v), img_red);
+  //
+  //   cv::imshow("Original", img);
+  //   cv::imshow("Red", img_red);
+  //   if(cv::waitKey(30) == 27) //press esc to exit
+  //     break;
+  // }
 
-    cv::imshow("Original", img);
-    cv::imshow("Red", img_red);
-    if(cv::waitKey(30) == 27) //press esc to exit
-      break;
-}
+  //For plinko.jpg
+  cv::cvtColor(img, hsv, cv::COLOR_BGR2HSV); //Easier to detect color in HSV
+  cv::inRange(hsv, cv::Scalar(0, 130, 215), cv::Scalar(40, 255, 255), img_red);
+  cv::inRange(hsv, cv::Scalar(95, 70, 185), cv::Scalar(135, 255, 255), img_blue);
+  cv::inRange(hsv, cv::Scalar(50, 80, 170), cv::Scalar(90, 180, 255), img_green);
+
+  // The following may not be necessary if our thresholding is good enough and we need to be faster
+  img_red = cleanUpNoise(img_red);
+  img_blue = cleanUpNoise(img_blue);
+  img_green = cleanUpNoise(img_green);
+
+  std::vector<cv::Point2f> red_center = findCentroids(img_red);
+  std::vector<cv::Point2f> blue_center = findCentroids(img_blue);
+  std::vector<cv::Point2f> green_center = findCentroids(img_green);
+
+  printCentroids(red_center);
+  printCentroids(blue_center);
+  printCentroids(green_center);
+
+  std::cout << red_center.size() << "\t" << blue_center.size() << "\t" << green_center.size() << "\n";
+
+  cv::imshow("Red", img_red);
+  cv::imshow("Blue", img_blue);
+  cv::imshow("Green", img_green);
+  cv::imshow("Original", img);
+  cv::waitKey();
 
   return 0;
 }
