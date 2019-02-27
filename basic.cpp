0dbd7e3c7f9ea46e1704d0b504f62018fa0af459
Brendon Forsgren <bforsgren29@gmail.com>
2019-02-27 14:21:45 -0700

    Copied Jaron's code into the basic.cpp file

--- basic.cpp
+++ basic.cpp
@@ -1,4 +1,5 @@
 #include <opencv2/opencv.hpp>
+#include <vector>
 
 
 
@@ -9,7 +10,62 @@
 
 int fd, n, i;
 char buf[128] = "temp text";
+int mouse_X, mouse_Y;
+std::vector<cv::Point> points;
 using namespace cv;
+
+void sendCommand(const char* command);
+int setupSerial();
+cv::Rect calibrate_camera(cv::Mat frame);
+void on_mouse(int evt, int x, int y, int flags, void* param);
+
+int main(int, char**)
+{   int frameCounter = 0;
+    Mat frameLast;
+    VideoCapture cap(0); // open the default camera
+    setupSerial();
+    if(!cap.isOpened())  // check if we succeeded
+        return -1;
+
+    cap >> frameLast;
+    sendCommand("h\n"); // Home the motor and encoder
+
+    //Setting up our calibration stuff here
+    cv::Rect calibrationRect(cv::Point(-1,-1),cv::Point(-1,-1));
+    calibrationRect = calibrate_camera(frameLast);
+
+    for(;;)
+    {
+        frameCounter++;
+        Mat frame;
+
+        cap >> frame; // get a new frame from camera
+        cv::rectangle(frame, calibrationRect, cv::Scalar(0, 255, 0));
+        
+	      if(!frame.empty()){
+        //ADD YOUR CODE HERE
+
+
+    		imshow("Camera Input", frame);
+    		if(waitKey(10) >= 0) break;
+
+        // Command structure is very simple
+        // "h\n" is to home the motor
+        // "g<integer range 7 to 53>\n" sends the motor to that position in cm
+        // e.g. "g35\n" sends the motor to 35cm from left wall
+        if(frameCounter%200==0)
+        {
+            sendCommand("g10\n");
+        }
+        else if(frameCounter%100==0)
+        {
+            sendCommand("g50\n");
+        }
+	}
+    }
+    // the camera will be deinitialized automatically in VideoCapture destructor
+    return 0;
+}
 
 void sendCommand(const char* command)
 {
@@ -54,44 +110,31 @@ int setupSerial()
     return 0;
 }
 
+cv::Rect calibrate_camera(cv::Mat frame)
+{
+  cv::namedWindow("ImageDisplay", 1);
+  cv::setMouseCallback("ImageDisplay", on_mouse, (void*)&points);
 
-int main(int, char**)
-{   int frameCounter = 0;
-    Mat frameLast;
-    VideoCapture cap(0); // open the default camera
-    setupSerial();
-    if(!cap.isOpened())  // check if we succeeded
-        return -1;
+  std::cout << "Please click on Top Left Corner. Then press space to Continue." << "\n";
+  cv::imshow("ImageDisplay", frame);
+  cv::waitKey(0);
+  cv::Point tl(mouse_X, mouse_Y);
+  std::cout << "Point: " << mouse_X << " " << mouse_Y << "\n";
 
-    cap >> frameLast;
-    sendCommand("h\n"); // Home the motor and encoder
-    for(;;)
-    {
-        frameCounter++;
-        Mat frame;
+  std::cout << "Please click on Bottom Right Corner. Then press space to Continue." << "\n";
+  cv::imshow("ImageDisplay", frame);
+  cv::waitKey(0);
+  cv::Point br(mouse_X, mouse_Y);
+  std::cout << "Point: " << mouse_X << " " << mouse_Y << "\n";
 
-        cap >> frame; // get a new frame from camera
-	      if(!frame.empty()){
-        //ADD YOUR CODE HERE
+  cv::destroyWindow("ImageDisplay");
+  return cv::Rect(tl,br);
+}
 
-
-    		imshow("Camera Input", frame);
-    		if(waitKey(10) >= 0) break;
-
-        // Command structure is very simple
-        // "h\n" is to home the motor
-        // "g<integer range 7 to 53>\n" sends the motor to that position in cm
-        // e.g. "g35\n" sends the motor to 35cm from left wall
-        if(frameCounter%200==0)
-        {
-            sendCommand("g10\n");
-        }
-        else if(frameCounter%100==0)
-        {
-            sendCommand("g50\n");
-        }
-	}
-    }
-    // the camera will be deinitialized automatically in VideoCapture destructor
-    return 0;
+void on_mouse(int evt, int x, int y, int flags, void* param)
+{
+   if(evt == cv::EVENT_LBUTTONDOWN) { //CV_EVENT_LBUTTONDOWN
+       mouse_X = x;
+       mouse_Y = y;
+   }
 }
